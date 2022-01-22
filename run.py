f9a853becc428ffb722eeffa8be6b3b19da81500
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-22 14:22:49 -0500

    Added the section for about Pinlock

---
+++ run.py
@@ -0,0 +1,18 @@
+
+
+def main():
+  print("WELCOME TO PINLock.")
+  print("Use the following numbers to select your choice of action.")
+  print("1)LOGIN\n 2)SIGN UP\n 3)ACCOUNTS DISPLAY\n 4) ABOUT PinLock\n 5)LOG OUT")
+
+  option = int(input())
+  if option == 4:
+    print("\nABOUT PINLock")
+    print("We are the leading security password lockers. We ensure that your passwords are stored safely and securely from hackers. We also help you have a systmatic way of accessing your passwords incase you don't remember them.")
+
+
+
+
+if __name__ == "__main__":
+  main()
+
