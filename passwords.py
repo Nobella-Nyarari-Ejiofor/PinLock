a44d59c8b87b456cfbded691645784254010f26f
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-22 21:55:38 -0500

    Created a functionality to add and remove passwords

--- passwords.py
+++ passwords.py
@@ -4,3 +4,18 @@ class Password:
     self.page = page 
 
 user_password= []
+
+def save_page(self):
+  """
+  Saves the password to the user_passwords array
+  """
+  Password.user_password.append(self)
+
+def remove_page(self):
+  """
+ Deletes a password  
+  """
+  Password.user_password.remove(self)
+
+
+
